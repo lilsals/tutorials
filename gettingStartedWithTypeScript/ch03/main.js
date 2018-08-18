1deaa3c93c49819f5d8696a550ac0781b6a35068
Lucas Salisbury <lucassalisbury@gmail.com>
2018-08-17 22:21:29 -0700

    initial upload

---
+++ gettingStartedWithTypeScript/ch03/main.js
@@ -0,0 +1,11 @@
+function printFirstNames(friends) {
+    for (let friend of friends) {
+        console.log(friend.firstName);
+    }
+}
+printFirstNames([
+    { firstName: "Lucas" },
+    { firstName: "Dan" },
+    { firstName: "Ashleigh" }
+]);
+//# sourceMappingURL=main.js.map
